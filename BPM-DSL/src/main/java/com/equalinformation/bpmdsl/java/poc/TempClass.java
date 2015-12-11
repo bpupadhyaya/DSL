fded8493f204171c8e7ee495aebccfed2327ae46
Bhim Upadhyaya <bpupadhyaya@gmail.com>
2015-12-10 19:46:44 -0800

    Initial project structure for BPM DSL.

---
+++ BPM-DSL/src/main/java/com/equalinformation/bpmdsl/java/poc/TempClass.java
@@ -0,0 +1,8 @@
+package com.equalinformation.bpmdsl.java.poc;
+
+/**
+ * Created by bpupadhyaya on 12/10/15.
+ */
+public class TempClass {
+    //TODO This is a temp class, delete later.
+}
---
+++ BPM-DSL/src/main/scala/com/equalinformation/bpmdsl/scala/poc/TempObject.scala
@@ -0,0 +1,8 @@
+package com.equalinformation.bpmdsl.scala.poc
+
+/**
+  * Created by bpupadhyaya on 12/10/15.
+  */
+object TempObject extends App {
+  println("This is a temp class, delete later")
+}
